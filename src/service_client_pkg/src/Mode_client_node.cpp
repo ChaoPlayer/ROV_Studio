d61e0b7132ce8bb195f14078ba70602f7aa346ca
ChaoPlayer <1847848758@qq.com>
2025-02-06 11:55:17 +0800

    2025.2.6，稍微完善了查看飞行器飞行状态的代码

--- src/service_client_pkg/src/Mode_client_node.cpp
+++ src/service_client_pkg/src/Mode_client_node.cpp
@@ -2,28 +2,34 @@
 #include<mavros/SetMode.h>
 #include<string>
 #include<ros/service_client.h>
+#include<mavros_msgs/State.h>
 
-uint8_t base_mode_get()
-{
-    //用于获取用户输入的控制基本类型
-}
-std::string custom_mode_get()
+std::string current_custom_mode;
+
+// uint8_t base_mode_get()
+// {
+//     //用于获取用户输入的控制基本类型
+// }
+void custom_mode_get(const mavros_msgs::State::ConstPtr& msg)
 {
     //用于获得用户输入的控制具体模式
+    current_custom_mode=msg->custom_mode;
 }
 
 
 int main(int argc,char **argv)
 {
     ros::init(argc,argv,"Mode_client_node");
-    ros::Nodehandle nh;
+    ros::Nodehandle nh,ns;
 
     ros::ServiceClient client=nh.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
     mavros_msgs::SetMode set_mode_srv;
+    ros::Subscriber mode_sub=mode_sub.subscribe("mode_state",10,custom_mode_get)
 
-    set_mode_srv.base_mode=base_mode_get();
-    set_mode_srv.custom_mode=custom_mode_get();
+    //set_mode_srv.base_mode=base_mode_get();
+    set_mode_srv.custom_mode=current_custom_mode;
     ROS_INFO("Current custom_mode is %s",set_mode_srv.custom_mode);
+
     if(client.call(set_mode_srv))
     {
         if(set_mode_srv.response.success)
--- src/service_client_pkg/src/Mode_server_node.cpp
+++ src/service_client_pkg/src/Mode_server_node.cpp
@@ -1,9 +1,52 @@
 #include<ros/ros.h>
 #include<service_client_pkg/ServiceClientExMsg.h>
 #include<string>
+#include<mavros_msgs/SetMode.h>
+#include<mavros/command.h>
+#include<mavros_msgs/State.h>
+#include<mavros/mavlink_convert.h>
 
-void mode_inquiry(service_client_pkg::ServiceClientExMsgRequest& request,service_client_pkg::ServiceClientExMsgResponse& response)
+ros::ServiceServer set_mode_service;
+ros::Publisher heartbeat_pub;
+ros::Publisher mode_pub;
+ros::Subscriber mode_sub;
+
+bool mode_change(mavros_msgs::SetMode::Request &req,mavros_msgs::SetMode::Response &res)
 {
-    string target_mode=request.mode_request;
-    v  
+    uint8_t base_mode=req.base_mode;
+    std:string custom_mode=req.custom_mode;
+    
+    mavlink_message_t msg;
+    mavlink_msg_set_mode_pack(1,0,base_mode,custom_mode,c_str());
+
+    mavros::mavlink::send_message(msg);
+
+    res.success=true;
+    res.result=0;
+
+    ROS_INFO("Successfully changed mode to %s",custom_mode.c_str());
+    return true;
+}
+
+void mode_state(const mavros_msgs::State::ConstPtr& msg)
+{
+    mavros_msgs::State custom_mode_msg;
+    custom_mode_msg.custom_mode=msg->custom_mode;
+    mode_pub.publish(custom_mode_msg);
+}
+
+int main(int argc,char **argv)
+{
+    ros::init(argc,argv,"Mode_server_node");
+    ros::Nodehandle n;
+    ros::Nodehandle nh;
+    mode_sub.subscribe("mavros/state",10,mode_state);
+    mode_pub.advertise<mavros_msgs::State>("mode_state",10);//发布当前custom_mode
+
+    set_mode_service=n.advertiseService("mavros/set_mode",mode_change);
+    heartbeat_pub=n.advertise<mavros_msgs::Heartbeat>("/mavros/heartbeat",10);
+
+    ROS_INFO("Set mode service is ready");
+    ros::spin();
+    return 0;
 }
